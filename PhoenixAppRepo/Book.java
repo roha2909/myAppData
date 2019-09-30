ba795c643425cd133c3d81b0468e3e8756b79005
roha2909 <55914174+roha2909@users.noreply.github.com>
2019-09-29 23:37:35 -0700

    Add files via upload

---
+++ PhoenixAppRepo/Book.java
@@ -0,0 +1,24 @@
+class Book 
+{
+private int bookId;
+private String name;
+
+public int getbookId()
+{return bookId;
+}
+system.out.println("BookID is set");
+public void setbookId()
+{
+	this.bookId=bookId;
+	return bookId;
+	
+}
+public void getName()
+{return name;
+}
+public void setName()
+{
+	this.bookName=bookName;
+	return bookName;
+}
+}
